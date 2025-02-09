e0bbe2a1d2dcbe4c5cb80466a2630eb3efd41142
ChrisGuter <christian.gutermann1337@gmail.com>
2025-02-10 00:59:35 +0100

    hero component -> styling  and typo fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { curve, hero, heroBackground, robot } from "../assets";
+import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
 import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
@@ -14,16 +14,16 @@ const Hero = () => {
 
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25]"
+      className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
       id="hero"
     >
       <div className="container relative" ref={parallaxRef}>
-        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibillities of&nbsp;AI&nbsp;chatting with {}
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -31,7 +31,7 @@ const Hero = () => {
                 className="absolute top-full left-0 w-full xl:-mt-2"
                 width={624}
                 height={28}
-                alt="curve"
+                alt="Curve"
               />
             </span>
           </h1>
@@ -47,6 +47,7 @@ const Hero = () => {
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                 <img
                   src={robot}
@@ -61,7 +62,7 @@ const Hero = () => {
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                     {heroIcons.map((icon, index) => (
-                      <li key={index} className="p5">
+                      <li className="p-5" key={index}>
                         <img src={icon} width={24} height={25} alt={icon} />
                       </li>
                     ))}
@@ -91,9 +92,11 @@ const Hero = () => {
 
           <BackgroundCircles />
         </div>
-        <CompanyLogos />
+
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
-      <BottomLine className="hidden relative z-10 mt-20 lg:block" />
+
+      <BottomLine />
     </Section>
   );
 };
